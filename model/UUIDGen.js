c21b1f37b850136feddbf73a2d6497505520dd6f
small_sunshine <jioo0224@naver.com>
2018-03-10 18:00:56 +0900

    소스 최적화

--- model/UUIDGen.js
+++ model/UUIDGen.js
@@ -1,20 +1,7 @@
 const uuidv4 = require('uuid/v4')
 
-module.exports = (db) => {
-  return new Promise((resolve, reject) => {
-    async function generate (db, resolve, reject) {
-      try {
-        const uuid = uuidv4()
-        const result = await db(uuid)
-        if (result) {
-          resolve(uuid)
-        } else {
-          generate(db, resolve, reject)
-        }
-      } catch (error) {
-        reject(error)
-      }
-    }
-    generate(db, resolve, reject)
-  })
+module.exports = async (db) => {
+  let uuid = uuidv4()
+  while (!await db(uuid)) uuid = uuidv4()
+  return uuid
 }
--- model/User.js
+++ model/User.js
@@ -4,85 +4,41 @@ const salt = 'soynote'
 const UUIDGen = require('./UUIDGen')
 module.exports.salt = salt
 
-module.exports.findById = (id) => {
-  return new Promise((resolve, reject) => {
-    db.User.findOne({
-      where: { id }
-    }).then((user) => {
-      if (user && user.dataValues) {
-        resolve(user.dataValues)
-      } else {
-        resolve(null)
-      }
-    }).catch((error) => {
-      reject(error)
-    })
+module.exports.findById = async (id) => {
+  let user = db.User.findOne({
+    where: { id }
   })
+  return (!!user) && user.dataValues
 }
 
-module.exports.findByEmail = (email) => {
-  return new Promise((resolve, reject) => {
-    db.User.findOne({
-      where: { email }
-    }).then((user) => {
-      if (user && user.dataValues) {
-        resolve(user.dataValues)
-      } else {
-        resolve(null)
-      }
-    }).catch((error) => {
-      reject(error)
-    })
+module.exports.findByEmail = async (email) => {
+  let user = await db.User.findOne({
+    where: { email }
   })
+  return (!!user) && user.dataValues
 }
 
-module.exports.findByNick = (nickname) => {
-  return new Promise((resolve, reject) => {
-    db.User.findOne({
-      where: { nickname }
-    }).then((user) => {
-      if (user && user.dataValues) {
-        resolve(user.dataValues)
-      } else {
-        resolve(null)
-      }
-    }).catch((error) => {
-      reject(error)
-    })
+module.exports.findByNick = async (nickname) => {
+  let user = await db.User.findOne({
+    where: { nickname }
   })
+  return (!!user) && user.dataValues
 }
 
-module.exports.register = ({email, password, nickname}) => {
+module.exports.register = async ({email, password, nickname}) => {
   const createPassword = require('./User').password
-  return new Promise((resolve, reject) => {
-    UUIDGen((result) => {
-      const findById = require('./User').findById
-      return new Promise((resolve, reject) => {
-        findById(result).then((result) => {
-          if (!result) {
-            resolve(true)
-          } else {
-            resolve(false)
-          }
-        }).catch((error) => {
-          reject(error)
-        })
-      })
-    }).then((uuid) => {
-      db.User.create({
-        uuid,
-        email,
-        password: createPassword(password, salt),
-        nickname
-      }).then(() => {
-        resolve(true)
-      }).catch((error) => {
-        reject(error)
-      })
-    }).error((error) => {
-      reject(error)
-    })
+  let uuid = await UUIDGen(async (result) => {
+    const findById = require('./User').findById
+    let find = await findById(result)
+    return !find
   })
+  await db.User.create({
+    uuid,
+    email,
+    password: createPassword(password, salt),
+    nickname
+  })
+  return true
 }
 
 module.exports.password = (password, salt) => {
