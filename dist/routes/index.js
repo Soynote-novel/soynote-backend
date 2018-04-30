e51f77f0f12ce3e309c273776950dc76aee50cca
small_sunshine <jioo0224@naver.com>
2018-05-01 00:19:42 +0900

    빌드파일 제거

--- dist/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_1 = require("./auth");
-exports.auth = auth_1.default;
-const oauth_1 = require("./oauth");
-exports.oauth = oauth_1.default;
--- dist/table/_Table.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const Sequelize = require('sequelize');
-const { database, username, password, config } = require('../config').db;
-const Table = new Sequelize(database, username, password, config);
-Table.sync();
-module.exports = Table;
